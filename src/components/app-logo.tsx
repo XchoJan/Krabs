d84af1ff3ca92c9783bc1a313f32d7942ea07705
Nodirbek Xushbaxtov <nodirbek.devoloper@gmail.com>
2025-04-26 23:25:40 +0500

    version upgraded

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {View} from "react-native";
-import Kraab from '../assets/svg/appLogoVectors/Kraab.svg'
-import Systems from '../assets/svg/appLogoVectors/Systems.svg'
+import {View} from 'react-native';
+import Kraab from '../assets/svg/appLogoVectors/Kraab.svg';
+import Systems from '../assets/svg/appLogoVectors/Systems.svg';
 const AppLogo = () => {
-    return (
-        <View>
-            <View style={{marginBottom: 9}}>
-                <Kraab/>
-            </View>
-            <View>
-                <Systems/>
-            </View>
-        </View>
-    );
+  return (
+    <View>
+      <View style={{marginBottom: 9}}>
+        <Kraab />
+      </View>
+
+      <View>
+        <Systems />
+      </View>
+    </View>
+  );
 };
 
 export default AppLogo;
